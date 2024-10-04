bc323fe72bdd88f716df220ecad88952d5b96543
Braulio Lomeli <brauliolomeli@gmail.com>
2024-10-04 17:16:28 -0500

    Revert "Refactor buildSlackAttachments function to include service information"

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const { context } = require("@actions/github");
 
-function buildSlackAttachments({ status, color, github, service, workflow }) {
+function buildSlackAttachments({ status, color, github }) {
   const { payload, ref, workflow, eventName } = github.context;
   const { owner, repo } = context.repo;
   const event = eventName;
@@ -17,24 +17,16 @@ function buildSlackAttachments({ status, color, github, service, workflow }) {
 
   const referenceLink =
     event === "pull_request"
-      ? [
-          {
-            title: "Pull Request",
-            value: `<${payload.pull_request.html_url} | ${payload.pull_request.title}>`,
-            short: true,
-          },
-        ]
-      : [];
-
-  const serviceAttachment = service
-    ? [
-        {
-          title: "Service",
-          value: service,
+      ? {
+          title: "Pull Request",
+          value: `<${payload.pull_request.html_url} | ${payload.pull_request.title}>`,
           short: true,
-        },
-      ]
-    : [];
+        }
+      : {
+          title: "Branch",
+          value: `<https://github.com/${owner}/${repo}/commit/${sha} | ${branch}>`,
+          short: true,
+        };
 
   return [
     {
@@ -55,13 +47,7 @@ function buildSlackAttachments({ status, color, github, service, workflow }) {
           value: status,
           short: true,
         },
-        ...referenceLink,
-        ...serviceAttachment,
-        {
-          title: "Branch",
-          value: `<https://github.com/${owner}/${repo}/commit/${sha} | ${branch}>`,
-          short: true,
-        },
+        referenceLink,
         {
           title: "Event",
           value: event,
